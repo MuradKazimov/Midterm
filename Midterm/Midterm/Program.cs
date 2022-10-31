2a6476473213dbba2820fc84853185080a483214
MuradKazimov <75727226+MuradKazimov@users.noreply.github.com>
2022-10-31 18:47:55 +0400

    Add files via upload

---
+++ Midterm/Midterm/Program.cs
@@ -0,0 +1,33 @@
+﻿// Adlari elifba sirasi ile duzmek
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Names = new List<string>();
+
+            Names.Add("Vaqif");
+            Names.Add("Zamiq");
+            Names.Add("Akif");
+            Names.Add("Kerim");
+            Names.Add("Qurban");
+            Names.Sort();
+            Console.WriteLine("Names in Alphabetical Order");
+            foreach (string n in Names)
+            {
+                Console.WriteLine(n);
+            }
+            Console.ReadLine();
+        }
+       
+    }
+}
